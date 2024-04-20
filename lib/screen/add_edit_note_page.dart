1bc6800556e6221b5d19237190157778386892c7
ekosarr <ekoafi123@gmail.com>
2024-04-20 11:08:38 +0700

    halaman edit delete page

--- lib/screen/add_edit_note_page.dart
+++ lib/screen/add_edit_note_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AddEditNotePage extends StatefulWidget {
-  const AddEditNotePage({super.key});
+  const AddEditNotePage({Key? key}) : super(key: key);
 
   @override
   State<AddEditNotePage> createState() => _AddEditNotePageState();
@@ -12,14 +12,47 @@ class _AddEditNotePageState extends State<AddEditNotePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Note List App',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
+        title: Row(
+          children: [
+            Text(
+              'Back',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ],
         ),
-        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () {
+              // Implementasi untuk tombol edit
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              // Implementasi untuk tombol hapus
+            },
+          ),
+        ],
         backgroundColor: Colors.white38,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter your note here...',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: null, 
+            ),
+          ],
+        ),
       ),
     );
   }
